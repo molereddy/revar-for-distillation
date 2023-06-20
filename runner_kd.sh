25584966d6b1e6b29853044c74db74c27385d24c
molereddy <m.anmolreddy@gmail.com>
2023-06-20 13:57:58 +0000

    getting latest param run scripts

--- runner_kd.sh
+++ runner_kd.sh
@@ -8,7 +8,7 @@ batch_size=200
 eval_batch_size=200
 
 epochs=400
-lr=0.1
+lr=0.05
 wd=5e-4
 momentum=0.9
 
@@ -41,11 +41,19 @@ teacher_name='ResNet10_l'
 
 
 echo "S=$base_name"
+TZ=IST-5:30 date;
 CUDA_VISIBLE_DEVICES="$GPU"  python train_kd.py --dataset $dataset --data_path $data_path \
     --teacher $teacher_name --model_name $base_name --save_dir $save_dir \
 	--eval_batch_size $eval_batch_size --batch_size $batch_size  \
 	--lr $lr --momentum $momentum --sched_cycles 2 \
 	--loss_kd_frac 0.9 --pretrained_student True \
-	--temperature 2 --epochs 400 --wd $wd --rand_seed $1
-
+	--temperature 2 --epochs 400 --wd $wd --rand_seed 1 > /dev/null;
+TZ=IST-5:30 date; 
+CUDA_VISIBLE_DEVICES="$GPU"  python train_kd.py --dataset $dataset --data_path $data_path \
+    --teacher $teacher_name --model_name $base_name --save_dir $save_dir \
+	--eval_batch_size $eval_batch_size --batch_size $batch_size  \
+	--lr $lr --momentum $momentum --sched_cycles 2 \
+	--loss_kd_frac 0.9 --pretrained_student True \
+	--temperature 2 --epochs 400 --wd $wd --rand_seed 2 > /dev/null;
+TZ=IST-5:30 date; 
 
--- runner_meta.sh
+++ runner_meta.sh
@@ -4,10 +4,10 @@ dataset='cifar100'
 data_path='/home/prathamesh/code/data/cifar/'
 save_dir='./meta_results/'
 
-batch_size=400
-eval_batch_size=400
+batch_size=200
+eval_batch_size=200
 
-epochs=200
+epochs=400
 lr=0.05
 wd=5e-4 # metascorer paper used weight decay of 1e-4
 momentum=0.9
@@ -48,8 +48,16 @@ echo "S=$base_name"
 # 	--lr $lr --wd $wd --momentum $momentum --epochs $epochs --meta_lr 0.01 \
 # 	--pretrained_student True --temperature 2 --sched_cycles 1 \
 # 	--rand_seed $1
+TZ=IST-5:30 date; 
 python train_meta.py --dataset $dataset --data_path $data_path \
     --teacher $teacher_name --model_name $base_name --save_dir $save_dir \
 	--eval_batch_size $eval_batch_size --batch_size $batch_size  \
-	--lr $lr --wd $wd --momentum $momentum --epochs 200 \
-	--temperature 2 --rand_seed $1
+	--lr $lr --wd $wd --momentum $momentum --epochs $epochs --meta_type "resnet" \
+	--temperature 2 --rand_seed 1 --sched_cycles 2 > /dev/null;
+TZ=IST-5:30 date;
+python train_meta.py --dataset $dataset --data_path $data_path \
+    --teacher $teacher_name --model_name $base_name --save_dir $save_dir \
+	--eval_batch_size $eval_batch_size --batch_size $batch_size  \
+	--lr $lr --wd $wd --momentum $momentum --epochs $epochs --meta_type "resnet" \
+	--temperature 2 --rand_seed 2 --sched_cycles 2 > /dev/null;
+TZ=IST-5:30 date;
